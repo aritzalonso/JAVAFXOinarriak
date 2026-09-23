05174bb11ba9b42a9969aae59ba52e9987cc629c
Aritz Alonso <aalonso.25@izarraitz.eus>
2026-09-23 09:42:33 +0200

    pain layouta

--- src/main/java/eus/adibidea/Main.java
+++ src/main/java/eus/adibidea/Main.java
@@ -2,14 +2,32 @@ package eus.adibidea;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start (Stage stage){
-        StackPane root = new StackPane();
+
+        Pane root = new Pane();
+        Label label = new Label("kaixo");
+        label.setLayoutX(100);
+        label.setLayoutY(100);
+        Button button = new Button("ados!");
+        button.setLayoutX(300);
+        button.setLayoutY(300);
+        root.getChildren().addAll(label,button);
+
+
+
+
+
+
         Scene scene = new Scene(root);
         stage.setTitle("JavaFX aplikazioa");
         stage.setScene(scene);
